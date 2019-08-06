40a79672bf69e4add9cda0ea2f4fb6ad6803c0e6
raojingpeng <withrjp@gmail.com>
2019-08-06 20:08:03 +0800

    modified footer 2

--- front-end/src/components/Footer.vue
+++ front-end/src/components/Footer.vue
@@ -33,6 +33,34 @@
           <v-img :src="require('@/assets/wechat.jpg')"></v-img>
         </v-tooltip>
 
+        <v-tooltip top min-width="250px" content-class="qq" nudge-top="8">
+          <template v-slot:activator="{ on }">
+            <v-btn
+              flat
+              icon
+              color="blue"
+              v-on="on"
+            >
+              <v-icon>fab fa-qq</v-icon>
+            </v-btn>
+          </template>
+          <v-img :src="require('@/assets/qq.jpg')"></v-img>
+        </v-tooltip>
+
+         <v-btn
+          flat
+          icon
+          color="red"
+        >
+          <v-icon>fab fa-weibo</v-icon>
+        </v-btn>
+         <v-btn
+          flat
+          icon
+        >
+          <v-icon>fab fa-github</v-icon>
+        </v-btn>
+
       </v-flex>
 
     </v-layout>
